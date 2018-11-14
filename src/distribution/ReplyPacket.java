52179ef4b325c6d9ca0c841f9a7a993d9a19364a
Rafael Albuquerque <rafaellsma@gmail.com>
2018-11-14 11:11:29 -0300

    add ReplyPacket

---
+++ src/distribution/ReplyPacket.java
@@ -0,0 +1,13 @@
+package distribution;
+
+public class ReplyPacket {
+    private Message message;
+
+    public Message getMessage() {
+        return message;
+    }
+
+    public void setMessage(Message message) {
+        this.message = message;
+    }
+}
