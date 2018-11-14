b6b7da52a8b975783b2dbe1ff3182b5521cdc089
rcrs3 <rcrs3@cin.ufpe.br>
2018-11-14 07:42:04 -0300

    Add QueueManager

---
+++ src/distribution/QueueManager.java
@@ -0,0 +1,34 @@
+package distribution;
+
+import java.util.HashMap;
+
+public class QueueManager {
+    private HashMap<String, Queue> queues = new HashMap<String, Queue>();
+
+    public QueueManager(String host, int port) {
+    }
+
+    public void createQueue(String queueName) {
+        queues.putIfAbsent(queueName, new Queue());
+    }
+
+    public void storeInQueue(Message msg) {
+        queues.get(msg.getHeader()).push(msg);
+    }
+
+    public boolean isLastMessage(String queueName, String hash) {
+        return queues.get(queueName).getCurrent() == hash;
+    }
+
+    public Message getMessage(String queueName, String hash) {
+        Queue queue = queues.get(queueName);
+
+        return queue.get(hash);
+    }
+
+    public Message getNextMessage(String queueName, String hash) {
+        Queue queue = queues.get(queueName);
+        assert(!isLastMessage(queueName, hash)): "Last message";
+        return queue.get(queue.getNext(hash));
+    }
+}
