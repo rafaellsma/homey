bbc078e104b7282e46d199849352cc0f2ada8c0c
Arthurlpgc <arthurlpgcosta@gmail.com>
2018-11-18 16:22:12 -0300

    add op to reply

--- src/distribution/ReplyPacket.java
+++ src/distribution/ReplyPacket.java
@@ -4,11 +4,19 @@ import java.io.Serializable;
 
 public class ReplyPacket implements Serializable {
     private Message message;
-    public ReplyPacket(Message message){
+    private OperationType operationType;
+
+    public ReplyPacket(Message message, OperationType operationType){
         this.message = message;
+        this.operationType = operationType;
     }
+    
     public Message getMessage() {
         return message;
+    }
+
+    public OperationType getOperationType() {
+        return operationType;
     }
 
     public void setMessage(Message message) {
